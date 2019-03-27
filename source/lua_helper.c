d37d380677e1f93d59559234755176297ffbc654
jam1garner <jam1.mcleod@hotmail.com>
2019-03-26 22:45:17 -0400

    Remove unneeded FindSymbol

--- source/lua_helper.c
+++ source/lua_helper.c
@@ -2,14 +2,9 @@
 #include "saltysd_core.h"
 #include "saltysd_ipc.h"
 #include "saltysd_dynamic.h"
-#include "acmd_imports.h"
-
-__int64_t (*lib_L2CAgent_pop_lua_stack)(__int64_t, int) = NULL;
+#include "l2c_imports.h"
 
 void get_lua_stack(__int64_t* l2c_agent, int index, __int64_t* l2c_val) {
-    if (lib_L2CAgent_pop_lua_stack == NULL) 
-        lib_L2CAgent_pop_lua_stack = (__int64_t (*)(__int64_t, int))(SaltySDCore_FindSymbol("_ZN3lib8L2CAgent13pop_lua_stackEi"));
-	
 	asm("mov x8, %x0" : : "r"(l2c_val) : "x8" );
     lib_L2CAgent_pop_lua_stack(l2c_agent, index);
 }
