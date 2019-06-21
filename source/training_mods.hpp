fd1e5c4f8627a133297cd8f1a8d353ef2753fd3c
jugeeya <jugeeya@live.com>
2019-06-21 14:10:30 -0700

    formatting

--- source/training_mods.hpp
+++ source/training_mods.hpp
@@ -134,34 +134,34 @@ u64 enable_transition_term_replace(u64 module_accessor, int transition_id) {
 
 namespace ControlModule {
 int get_attack_air_kind_replace(u64 module_accessor) {
-	// call original
+    // call original
     u64 control_module = load_module(module_accessor, 0x48);
     int (*get_attack_air_kind)(u64) =
         (int (*)(u64))load_module_impl(control_module, 0x3B0);
     int kind = get_attack_air_kind(control_module);
 
-	if (is_training_mode() && is_operation_cpu(module_accessor)) {
-		if (TOGGLE_STATE == MASH_ATTACK) {
-			switch (ATTACK_STATE) {
-				case MASH_NAIR:
-					kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_N; break;
-				case MASH_FAIR:
-					kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_F; break;
-				case MASH_BAIR:
-					kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_B; break;
-				case MASH_UPAIR:
-					kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_HI; break;
-				case MASH_DAIR:
-					kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_LW; break;
-			}
-		}
+    if (is_training_mode() && is_operation_cpu(module_accessor)) {
+        if (TOGGLE_STATE == MASH_ATTACK) {
+            switch (ATTACK_STATE) {
+                case MASH_NAIR:
+                    kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_N; break;
+                case MASH_FAIR:
+                    kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_F; break;
+                case MASH_BAIR:
+                    kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_B; break;
+                case MASH_UPAIR:
+                    kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_HI; break;
+                case MASH_DAIR:
+                    kind = FIGHTER_COMMAND_ATTACK_AIR_KIND_LW; break;
+            }
+        }
 
         if (TOGGLE_STATE == MASH_RANDOM) {
             kind = app::sv_math::rand(hash40("fighter"), 5) + 1;
         }
-	}
+    }
 
-	return kind;
+    return kind;
 }
 
 int get_command_flag_cat_replace(u64 module_accessor, int category) {
@@ -185,10 +185,10 @@ int get_command_flag_cat_replace(u64 module_accessor, int category) {
                 if (category == FIGHTER_PAD_COMMAND_CATEGORY1) {
                     switch (ATTACK_STATE) {
                         case MASH_NAIR:
-						case MASH_FAIR:
-						case MASH_BAIR:
-						case MASH_UPAIR:
-						case MASH_DAIR:
+                        case MASH_FAIR:
+                        case MASH_BAIR:
+                        case MASH_UPAIR:
+                        case MASH_DAIR:
                             flag |= FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N; break;
                         case MASH_NEUTRAL_B:
                             flag |= FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_N; break;
@@ -206,12 +206,12 @@ int get_command_flag_cat_replace(u64 module_accessor, int category) {
                     int random_commands[] = {
                         FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE,
                         FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON,
-						// one for each aerial
+                        // one for each aerial
                         FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-						FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-						FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-						FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-						FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                        FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                        FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                        FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                        FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
                         FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_N,
                         FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
                         FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
