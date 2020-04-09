11cc29058e6ae129424a4ca9af4dfcefecf8d17b
jam1garner <jam1.mcleod@hotmail.com>
2020-04-09 05:10:23 -0400

    Fix some broken mangled symbols

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 #![no_std]
 #![feature(proc_macro_hygiene)]
 
-use skyline::{
-    libc::{fopen, FileOpenMode, fwrite_slice, fclose},
-    c_str
-};
+use skyline::nn::account::{self, Uid, GetLastOpenedUser, GetNickname, Nickname};
 
 #[skyline::main]
 pub fn main() {
@@ -14,12 +11,33 @@ pub fn main() {
         println!("{}", i);
     }
 
-    println!("Writing to file!");
-    
-    write_to_file("sd:/test.txt\0", "test test test test");
-    
-    println!("Done writing to file!");
+    init_accounts();
+
+    let nickname = unsafe { get_last_user_nickname() };
+
+    println!("Last nickname: {}", nickname);
 }
+
+fn init_accounts() {
+    unsafe { account::Initialize() };
+}
+
+unsafe fn get_last_user_nickname() -> Nickname {
+    let uid = &mut Uid::new();
+    let mut nick = Nickname::new();
+
+    GetLastOpenedUser(uid);
+    GetNickname(&mut nick, uid);
+
+    nick
+}
+
+
+/*
+use skyline::{
+    libc::{fopen, FileOpenMode, fwrite_slice, fclose},
+    c_str
+};
 
 fn write_to_file(file: &str, contents: &str) {
     unsafe {
@@ -27,4 +45,4 @@ fn write_to_file(file: &str, contents: &str) {
         fwrite_slice(contents.as_bytes(), file);
         fclose(file);
     }
-}
+}*/
