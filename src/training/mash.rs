d4269eb6a2a878fba58b768e1a18e8e2051e1c93
sidschingis <SijingYou92@gmail.com>
2020-06-06 17:19:48 +0200

    Extract Shared Condition
    Check against category is done by all matches so we can just check it before matching

--- src/training/mash.rs
+++ src/training/mash.rs
@@ -32,8 +32,12 @@ pub unsafe fn get_command_flag_cat(
     if !is_training_mode() {
         return;
     }
-    
+
     if !is_operation_cpu(module_accessor) {
+        return;
+    }
+
+    if category != FIGHTER_PAD_COMMAND_CATEGORY1 {
         return;
     }
 
@@ -47,92 +51,84 @@ pub unsafe fn get_command_flag_cat(
 
     match MENU.mash_state {
         Mash::Airdodge => {
-            if category == FIGHTER_PAD_COMMAND_CATEGORY1 {
-                *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE;
-            }
+            *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE;
         }
         Mash::Jump => {
-            if !is_in_landing(module_accessor) && category == FIGHTER_PAD_COMMAND_CATEGORY1 {
+            if !is_in_landing(module_accessor) {
                 *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON;
             }
         }
         Mash::Spotdodge => {
-            if category == FIGHTER_PAD_COMMAND_CATEGORY1 {
-                *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE;
-            }
+            *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE;
         }
         Mash::Attack => {
-            if category == FIGHTER_PAD_COMMAND_CATEGORY1 {
-                use Attack::*;
+            use Attack::*;
 
-                match MENU.mash_attack_state {
-                    Nair | Fair | Bair | UpAir | Dair => {
-                        *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N;
-                        // If we are performing the attack OOS we also need to jump
-                        if is_in_shieldstun(module_accessor) {
-                            *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON;
-                        }
+            match MENU.mash_attack_state {
+                Nair | Fair | Bair | UpAir | Dair => {
+                    *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N;
+                    // If we are performing the attack OOS we also need to jump
+                    if is_in_shieldstun(module_accessor) {
+                        *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON;
                     }
-                    NeutralB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_N,
-                    SideB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
-                    UpB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
-                    DownB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW,
-                    UpSmash => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_HI4,
-                    Grab => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_CATCH,
                 }
+                NeutralB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_N,
+                SideB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
+                UpB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
+                DownB => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW,
+                UpSmash => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_HI4,
+                Grab => *flag |= *FIGHTER_PAD_CMD_CAT1_FLAG_CATCH,
             }
         }
         Mash::Random => {
-            if category == FIGHTER_PAD_COMMAND_CATEGORY1 {
-                let situation_kind = StatusModule::situation_kind(module_accessor) as i32;
+            let situation_kind = StatusModule::situation_kind(module_accessor) as i32;
 
-                if situation_kind == SITUATION_KIND_AIR {
-                    let random_commands = vec![
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON,
-                        // one for each aerial
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW,
-                    ];
+            if situation_kind == SITUATION_KIND_AIR {
+                let random_commands = vec![
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_AIR_ESCAPE,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON,
+                    // one for each aerial
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW,
+                ];
 
-                    let random_cmd_index =
-                        app::sv_math::rand(hash40("fighter"), random_commands.len() as i32)
-                            as usize;
+                let random_cmd_index =
+                    app::sv_math::rand(hash40("fighter"), random_commands.len() as i32)
+                        as usize;
 
-                    *flag |= random_commands[random_cmd_index];
-                } else if situation_kind == SITUATION_KIND_GROUND {
-                    let random_commands = vec![
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_S3,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_HI3,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_LW3,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_S4,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_HI4,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_LW4,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_CATCH,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE_F,
-                        *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE_B,
-                    ];
+                *flag |= random_commands[random_cmd_index];
+            } else if situation_kind == SITUATION_KIND_GROUND {
+                let random_commands = vec![
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_N,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_S3,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_HI3,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_LW3,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_S4,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_HI4,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ATTACK_LW4,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_S,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_HI,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_SPECIAL_LW,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_CATCH,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE_F,
+                    *FIGHTER_PAD_CMD_CAT1_FLAG_ESCAPE_B,
+                ];
 
-                    let random_cmd_index =
-                        app::sv_math::rand(hash40("fighter"), random_commands.len() as i32)
-                            as usize;
+                let random_cmd_index =
+                    app::sv_math::rand(hash40("fighter"), random_commands.len() as i32)
+                        as usize;
 
-                    *flag |= random_commands[random_cmd_index];
-                }
+                *flag |= random_commands[random_cmd_index];
             }
         }
         _ => (),
@@ -161,7 +157,7 @@ pub unsafe fn check_button_on(
 
     // Buffer only in hitstun/landing/footstool
     if !is_in_hitstun(module_accessor)
-    && !is_in_landing(module_accessor) 
+    && !is_in_landing(module_accessor)
     && !is_in_footstool(module_accessor){
         return None;
     }
