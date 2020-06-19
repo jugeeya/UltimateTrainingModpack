64a9b0bc8bdb75f7506d740de59798ac669ad5af
sidschingis <SijingYou92@gmail.com>
2020-06-19 23:08:26 +0200

    Add Fast Fall Toggle
    Spark effect is currently missing

--- src/common/consts.rs
+++ src/common/consts.rs
@@ -50,7 +50,6 @@ impl From<i32> for Direction {
     }
 }
 
-
 //pub static FIGHTER_FACING_LEFT: f32 = 1.0;
 pub static FIGHTER_FACING_RIGHT: f32 = -1.0;
 pub static ANGLE_NONE: f64 = -69.0;
@@ -256,6 +255,13 @@ pub enum MashInNeutral {
     On = 1,
 }
 
+#[repr(i32)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum FastFall {
+    Off = 0,
+    On = 1,
+}
+
 #[repr(C)]
 pub struct TrainingModpackMenu {
     pub hitbox_vis: HitboxVisualization,
@@ -268,5 +274,6 @@ pub struct TrainingModpackMenu {
     pub shield_state: Shield,
     pub defensive_state: Defensive,
     pub oos_offset: i32,
-    pub mash_in_neutral: MashInNeutral
+    pub mash_in_neutral: MashInNeutral,
+    pub fast_fall: FastFall,
 }
---
+++ src/training/fast_fall.rs
@@ -0,0 +1,83 @@
+use crate::common::consts::FastFall;
+use crate::common::*;
+use smash::app::{self, lua_bind::*};
+use smash::lib::lua_const::*;
+use smash::phx::{Hash40, Vector3f};
+
+pub unsafe fn get_command_flag_cat(
+    module_accessor: &mut app::BattleObjectModuleAccessor,
+    category: i32,
+) {
+    if !is_training_mode() {
+        return;
+    }
+
+    // Once per frame
+    if category != 0 {
+        return;
+    }
+
+    if !is_operation_cpu(module_accessor) {
+        return;
+    }
+
+    if !is_airborne(module_accessor) {
+        return;
+    }
+
+    if MENU.fast_fall != FastFall::On {
+        return;
+    }
+
+    let y_speed =
+        KineticModule::get_sum_speed_y(module_accessor, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
+    // Need to be falling
+    if y_speed >= 0.0 {
+        return;
+    }
+
+    // Can't fastfall in hitstun
+    if is_in_hitstun(module_accessor) {
+        return;
+    }
+
+    // Already in fast fall, nothing to do
+    if WorkModule::is_flag(module_accessor, *FIGHTER_STATUS_WORK_ID_FLAG_RESERVE_DIVE) {
+        return;
+    }
+
+    // Set Fast Fall Flag
+    WorkModule::set_flag(
+        module_accessor,
+        true,
+        *FIGHTER_STATUS_WORK_ID_FLAG_RESERVE_DIVE,
+    );
+
+    // add_spark_effect(module_accessor);
+}
+
+unsafe fn add_spark_effect(module_accessor: &mut app::BattleObjectModuleAccessor) {
+    // Mock Spark effect
+    let pos = Vector3f {
+        x: PostureModule::pos_x(module_accessor),
+        y: PostureModule::pos_y(module_accessor),
+        z: 0.0,
+    };
+
+    let zeros = Vector3f {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    EffectModule::req(
+        module_accessor,
+        Hash40::new("sys_dragoon_flash"),
+        &pos,
+        &zeros,
+        1.0,
+        0,
+        0,
+        true,
+        0,
+    );
+}
