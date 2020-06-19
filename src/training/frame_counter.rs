fda63d946413440d36fc17d8bbb815088feb4d28
sidschingis <SijingYou92@gmail.com>
2020-06-20 00:26:31 +0200

    Allow Multiple Frame Counters

--- src/training/frame_counter.rs
+++ src/training/frame_counter.rs
@@ -1,30 +1,40 @@
 use crate::common::*;
 use crate::training::*;
 
-static mut CURRENT_FRAME : u32 = 0;
-static mut SHOULD_COUNT: bool = false;
+static mut SHOULD_COUNT: Vec<bool> = vec![];
+static mut COUNTERS: Vec<u32> = vec![];
 
+pub unsafe fn register_counter() -> usize {
+    let index = COUNTERS.len();
 
-pub unsafe fn start_counting(){
-    SHOULD_COUNT = true;
+    COUNTERS.push(0);
+    SHOULD_COUNT.push(false);
+
+    index
 }
 
-pub unsafe fn stop_counting(){
-    SHOULD_COUNT = false;
+pub unsafe fn start_counting(index:usize) {
+    SHOULD_COUNT[index] = true;
 }
 
-pub unsafe fn reset_frame_count(){
-    CURRENT_FRAME = 0;
+pub unsafe fn stop_counting(index:usize) {
+    SHOULD_COUNT[index] =  false;
 }
 
-pub unsafe fn get_frame_count() -> u32 {
-    CURRENT_FRAME
+pub unsafe fn reset_frame_count(index:usize) {
+    COUNTERS[index] = 0;
 }
 
-pub unsafe fn tick(){
-    if SHOULD_COUNT{
-        CURRENT_FRAME += 1;
-        println!("Tick {}",CURRENT_FRAME);
+pub unsafe fn get_frame_count(index:usize) -> u32 {
+    COUNTERS[index]
+}
+
+pub unsafe fn tick() {
+    for (index, _frame) in COUNTERS.iter().enumerate() {
+        if !SHOULD_COUNT[index]{
+            continue;
+        }
+        COUNTERS[index] += 1;
     }
 }
 
@@ -36,9 +46,15 @@ pub unsafe fn get_command_flag_cat(
         return;
     }
 
-    if !once_per_frame(module_accessor, category){
+    // Only do once per frame
+    if category != 0 {
         return;
     }
+
+    if !is_operation_cpu(module_accessor) {
+        return;
+    }
+    //~
 
     tick();
 }
