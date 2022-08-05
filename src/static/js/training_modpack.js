16f975a9d0314afd059a75a31aca87965889acde
Matthew Edell <64813455+xhudaman@users.noreply.github.com>
2022-08-04 22:34:25 -0400

    Update web UI for better maintenance (#380)
    * Updated menu buttons and icons
    
    * Cleaned up .devcotainer
    
    * removed dockerfile from .devcontainer in favour
    of using image
    
    * Added JS settings, updated menu template, css, js
    
    * Updated tab width to 4
    
    * Refactored openTab and closeAllItems,
    default tab content to be hidden,
    added polyfill for NodeList.forEach
    
    * Refactored openItem, made targeting modals easier
    
    * Refactored toggleOption
    
    * Fixed clicking tab not closing active modals
    
    * Updated aerial delay and falling aerial icons
    
    * Updated colours of defensive settings menu icons
    
    * Updated misc tab menu icon colours
    
    * Fixed oos_offset, shield_state, and shield_tilt
    
    * Fixed all tabs defaulting to active,
    Updated modals to be outside menu_items,
    Refactored JS to work with new HTML hierarchy
    
    * Updated settings to be JSON
    
    * Fixed single options not populating correctly from url
    
    * Updated menu to edit settings object when toggled
    
    * Removed updating settings from menu before exit
    
    * Refactored comments to fewer lines
    
    * Refactored to use const,
    Refactored to use camelCase,
    Refactored resetCurrentSubmenu & resetAllSubmenus
    
    * Fixed size and alignment of menu item text,
    Fixed size of option button's text,
    Fixed colour of crouch icon
    
    * Updated stroke width for crouch icon feet & arms
    
    * Updated check to use fontawesome as a base,
    Changed colour to have better contrast,
    Added outline for better contrast
    
    * Updated one-winged angel option to 1-winged angel
    
    * Updated openTab and resetCurrentMenu to play sfx

--- src/static/js/training_modpack.js
+++ src/static/js/training_modpack.js
@@ -1,321 +1,347 @@
-var isNx = (typeof window.nx !== 'undefined');
-var defaults_prefix = "__";
+// Polyfill for NodeList.forEach.
+// Allows forEach to be called directly on a node list (return type of doucment.querySelectorAll)
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
 
+// Polyfill for Object.entries
+// Iterates on an object and returns an array containing arrays of key/value pairs ([key, value])
+// for each pair in the object
+if (!Object.entries) {
+    Object.entries = function (obj) {
+        var ownProps = Object.keys(obj),
+            i = ownProps.length,
+            resArray = new Array(i); // preallocate the Array
+        while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+        return resArray;
+    };
+}
+
+const isNx = typeof window.nx !== 'undefined';
+var DEFAULTS_PREFIX = '__';
+
+// Set input handlers
 if (isNx) {
-    window.nx.footer.setAssign('B', '', close_or_exit, {se: ''});
-    window.nx.footer.setAssign('X', '', resetCurrentSubmenu, {se: ''});
-    window.nx.footer.setAssign('L', '', resetAllSubmenus, {se: ''});
-    window.nx.footer.setAssign('R', '', saveDefaults, {se: ''});
-    window.nx.footer.setAssign('ZR', '', cycleNextTab, {se: ''});
-    window.nx.footer.setAssign('ZL', '', cyclePrevTab, {se: ''});
+    window.nx.footer.setAssign('B', '', closeOrExit, { se: '' });
+    window.nx.footer.setAssign('X', '', resetCurrentMenu, { se: '' });
+    window.nx.footer.setAssign('L', '', resetAllMenus, { se: '' });
+    window.nx.footer.setAssign('R', '', saveDefaults, { se: '' });
+    window.nx.footer.setAssign('ZR', '', cycleNextTab, { se: '' });
+    window.nx.footer.setAssign('ZL', '', cyclePrevTab, { se: '' });
 } else {
     document.addEventListener('keypress', (event) => {
         switch (event.key) {
-            case "b":
-                console.log("b");
-                close_or_exit();
+            case 'b':
+                console.log('b');
+                closeOrExit();
                 break;
-            case "x":
-                console.log("x");
-                resetCurrentSubmenu();
+            case 'x':
+                console.log('x');
+                resetCurrentMenu();
                 break;
-            case "l":
-                console.log("l");
+            case 'l':
+                console.log('l');
                 resetAllSubmenus();
                 break;
-            case "r":
-                console.log("r");
+            case 'r':
+                console.log('r');
                 saveDefaults();
                 break;
-            case "p":
-                console.log("p");
+            case 'p':
+                console.log('p');
                 cycleNextTab();
                 break;
-            case "o":
-                console.log("o");
+            case 'o':
+                console.log('o');
                 cyclePrevTab();
                 break;
         }
     });
 }
 
-window.onload = onLoad;
-var settings = new Map();
-var lastFocusedItem = document.querySelector(".menu-item > button");
-
-function onLoad() {
+const onLoad = () => {
     // Activate the first tab
-    openTab(document.querySelector("button.tab-button"));
+    openTab(document.querySelector('button.tab-button'));
 
     // Extract URL params and set appropriate settings
     setSettingsFromURL();
-    setSubmenusFromSettings();
-}
+    populateMenuFromSettings();
+};
 
-function openTab(e) {
-    var tab_id = e.id.replace("button", "tab");
-    var selected_tab = document.getElementById(tab_id);
+window.onload = onLoad;
 
+var settings;
 
-    // Hide all content for all tabs
-    closeAllItems();
-    tabcontent = document.getElementsByClassName("tab-content");
-    Array.from(tabcontent).forEach(element => {
-        element.classList.add("hide");
-    });
+var lastFocusedItem = document.querySelector('.menu-item > button');
+const currentTabContent = () => {
+    const currentActiveTab = document.querySelector('.tab-button.active');
 
+    var currentActiveTabContent = document.querySelector(`#${currentActiveTab.id.replace('button', 'tab')}`);
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tab-button");
-    Array.from(tablinks).forEach(element => {
-        element.classList.remove("active");
-    });
+    return currentActiveTabContent;
+};
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    e.classList.add("active");
-    selected_tab.classList.remove("hide");
-    selected_tab.querySelector("button").focus();
-}
+const openTab = (eventTarget) => {
+    playSound('SeWebZoomIn');
+    const selectedTab = document.getElementById(eventTarget.id.replace('button', 'tab'));
+    const activeTabContent = document.querySelector('.tab-content:not(.hide)');
+    const activeTab = document.querySelector('.tab-button.active');
 
-function openItem(e) {
-    playSound("SeWebMenuListOpen");
-    var modal = e.parentElement.querySelector(".modal");
-    modal.classList.toggle("hide");
-    modal.querySelector("button").focus();
-    lastFocusedItem = e;
-}
+    // Hide content of current active tab
+    if (activeTabContent) {
+        activeTabContent.classList.add('hide');
+    }
 
-function closeAllItems() {
-    var modals = document.querySelectorAll(".modal");
-    Array.from(modals).forEach(element => {
-        element.classList.add("hide");
+    closeAllActiveModals();
+
+    // Remove "active" class from current active tab
+    if (activeTab) {
+        activeTab.classList.remove('active');
+    }
+
+    // Show the new current tab, and add an "active" class to the button that opened the tab
+    eventTarget.classList.add('active');
+    selectedTab.classList.remove('hide');
+    selectedTab.querySelector('button').focus();
+};
+
+const openMenuItem = (eventTarget) => {
+    playSound('SeWebMenuListOpen');
+
+    var { target } = eventTarget.dataset;
+    const modal = document.querySelector(`.modal[data-id=${target}]`);
+
+    currentTabContent().classList.toggle('hide');
+
+    modal.classList.toggle('hide');
+    modal.querySelector('button').focus();
+
+    lastFocusedItem = eventTarget;
+};
+
+const closeAllActiveModals = () => {
+    document.querySelectorAll('.modal:not(.hide)').forEach((modal) => {
+        modal.classList.add('hide');
     });
     lastFocusedItem.focus();
-}
+};
 
-function toggleOption(e) {
-    playSound("SeSelectCheck");
-    if (e.parentElement.classList.contains("single-option")) {
-        selectSingleOption(e);
-    } else {
-        var img = e.querySelector("img");
-        img.classList.toggle("hide");
-    }
-}
+const toggleOption = (element) => {
+    playSound('SeSelectCheck');
 
-function closestClass(elem, class_) {
-    // Returns the closest ancestor (including self) with the given class
-    // TODO: Consider removing
-    if (!elem) {
-        // Reached the end of the DOM
-        return null
-    } else if (elem.classList.contains(class_)) {
-        // Found it
-        return elem
-    } else {
-        // Didn't find it, go up a level
-        return closestClass(elem.parentElement, class_);
+    if (element.parentElement.classList.contains('single-option')) {
+        selectSingleOption(element);
+        return;
     }
-}
+
+    const img = element.querySelector('img');
+    const previouslySelected = !img.classList.contains('hidden');
+    const menuId = element.parentElement.dataset.id;
+    const toggleValue = parseInt(img.dataset.val);
+
+    settings[menuId] = previouslySelected ? settings[menuId] - toggleValue : settings[menuId] + toggleValue;
+
+    element.querySelector('img').classList.toggle('hidden');
+};
+
+// Add this later
+// function toggleSingleOption(element) {
+//     selectSingleOption(element);
+// }
+
+const closestClass = (element, class_) => {
+    if (!element) {
+        return null;
+    }
+
+    if (element.classList.contains(class_)) {
+        return element;
+    }
+
+    // Didn't find it, go up a level
+    return closestClass(element.parentElement, class_);
+};
+
 function playSound(label) {
-    // Valid labels:
-    // SeToggleBtnFocus
-    // SeToggleBtnOn
-    // SeToggleBtnOff
-    // SeCheckboxFocus
-    // SeCheckboxOn
-    // SeCheckboxOff
-    // SeRadioBtnFocus
-    // SeRadioBtnOn
-    // SeSelectCheck
-    // SeSelectUncheck
-    // SeBtnDecide
-    // SeTouchUnfocus
-    // SeBtnFocus
-    // SeKeyError
-    // SeDialogOpen
-    // SeWebZoomOut
-    // SeWebZoomIn
-    // SeWebNaviFocus
-    // SeWebPointerFocus
-    // SeFooterFocus
-    // SeFooterDecideBack
-    // SeFooterDecideFinish
-    // SeWebChangeCursorPointer
-    // SeWebTouchFocus
-    // SeWebLinkDecide
-    // SeWebTextboxStartEdit
-    // SeWebButtonDecide
-    // SeWebRadioBtnOn
-    // SeWebCheckboxUncheck
-    // SeWebCheckboxCheck
-    // SeWebMenuListOpen
+    //** Valid labels **//
+    // SeToggleBtnFocus, SeToggleBtnOn, SeToggleBtnOff, SeCheckboxFocus, SeCheckboxOn
+    // SeCheckboxOff, SeRadioBtnFocus, SeRadioBtnOn, SeSelectCheck, SeSelectUncheck, SeBtnDecide
+    // SeTouchUnfocus, SeBtnFocus, SeKeyError, SeDialogOpen, SeWebZoomOut, SeWebZoomIn, SeWebNaviFocus
+    // SeWebPointerFocus, SeFooterFocus, SeFooterDecideBack, SeFooterDecideFinish, SeWebChangeCursorPointer
+    // SeWebTouchFocus, SeWebLinkDecide, SeWebTextboxStartEdit, SeWebButtonDecide, SeWebRadioBtnOn
+    // SeWebCheckboxUncheck, SeWebCheckboxCheck, SeWebMenuListOpen
+
     if (isNx) {
         window.nx.playSystemSe(label);
     } else {
-        console.log("Sound Effect: " + label);
+        console.log('Sound Effect: ' + label);
     }
 }
 
-function exit() {
-    playSound("SeFooterDecideBack");
-    setSettingsFromMenu();
-    var url = buildURLFromSettings();
+const exit = () => {
+    playSound('SeFooterDecideBack');
+
+    const url = buildURLFromSettings();
 
     if (isNx) {
         window.location.href = url;
     } else {
         console.log(url);
     }
-}
+};
 
-function close_or_exit() {
-    // If any submenus are open, close them
-    // Otherwise if all submenus are closed, exit the menu and return to the game
-
-    if (document.querySelector(".modal:not(.hide)")) {
-        // Close any open submenus
-        console.log("Closing Items");
-        closeAllItems();
-    } else {
-        // If all submenus are closed, exit and return through localhost
-        console.log("Exiting");
-        exit();
+function closeOrExit() {
+    // Close any open menus
+    if (document.querySelector('.modal:not(.hide)')) {
+        console.log('Closing Items');
+        closeAllActiveModals();
+        currentTabContent().classList.remove('hide');
+        lastFocusedItem.focus();
+        return;
     }
+
+    console.log('Exiting');
+    exit();
 }
 
 function setSettingsFromURL() {
-    var regex = /[?&]([^=#]+)=([^&#]*)/g,
-        match;
-    while (match = regex.exec(document.URL)) {
-        settings.set(match[1], match[2]);
-    }
-}
+    var { search } = window.location;
+    const settingsFromSearch = search
+        .replace('?', '')
+        .split('&')
+        .reduce((accumulator, currentValue) => {
+            var [key, value] = currentValue.split('=');
+            accumulator[key] = parseInt(value);
+            return accumulator;
+        }, {});
 
-function setSettingsFromMenu() {
-    var section;
-    var mask;
-    [].forEach.call(document.querySelectorAll(".menu-item"), function (menuItem) {
-        section = menuItem.id;
-        mask = getMaskFromSubmenu(menuItem);
-        settings.set(section, mask);
-    });
+    settings = settingsFromSearch;
 }
 
 function buildURLFromSettings() {
-    var url = "http://localhost/";
-    url += "?";
-    settings.forEach((val, key) => { url += key + "=" + String(val) + "&" } );
-    return url
+    const url = 'http://localhost/?';
+
+    const urlParams = Object.entries(settings).map((setting) => {
+        return `${setting[0]}=${setting[1]}`;
+    });
+
+    return url + urlParams.join('&');
 }
 
-function selectSingleOption(e) {
+function selectSingleOption(eventTarget) {
     // Deselect all options in the submenu
-    parent = closestClass(e, "single-option");
-    siblings = parent.querySelectorAll(".menu-icon img");
-    [].forEach.call(siblings, function (sibling) {
-        sibling.classList.add("hide");
+    const parent = eventTarget.parentElement;
+
+    parent.querySelectorAll('.menu-icon:not(.hidden)').forEach((sibling) => {
+        sibling.classList.add('hidden');
+        settings[parent.dataset.id] = settings[parent.dataset.id] - parseInt(sibling.dataset.val);
     });
-    e.querySelector(".menu-icon img").classList.remove("hide");
+
+    eventTarget.querySelector('.menu-icon').classList.remove('hidden');
+
+    const value = parseInt(eventTarget.querySelector('.menu-icon').dataset.val);
+
+    settings[parent.dataset.id] = settings[parent.dataset.id] + value;
 }
 
-function setSubmenusFromSettings() {
-    [].forEach.call(document.querySelectorAll(".menu-item"), function (menuItem) {
-        var section = menuItem.id;
-        var section_mask = decodeURIComponent(settings.get(section));
-        setSubmenuByMask(menuItem, section_mask)
-    });
-}
+const isValueInBitmask = (value, mask) => (mask & value) != 0;
 
-function setSubmenuByMask(menuItem, mask) {
-    [].forEach.call(menuItem.querySelectorAll(".modal .menu-icon img"), function (toggle) {
-        if (isInBitmask(toggle.dataset.val, mask)) {
-            toggle.classList.remove("hide");
+const setOptionsForMenu = (menuId) => {
+    const modal = document.querySelector(`.modal[data-id="${menuId}"]`);
+
+    modal.querySelectorAll('.menu-icon').forEach(function (toggle) {
+        if (isValueInBitmask(toggle.dataset.val, settings[menuId])) {
+            toggle.classList.remove('hidden');
         } else {
-            toggle.classList.add("hide");
+            toggle.classList.add('hidden');
         }
     });
 
-    // If no setting for a Single Option is set, select the first one
-    var isSingleOption = menuItem.querySelectorAll(".modal.single-option").length != 0;
-    var isAllDeselected = menuItem.querySelectorAll(".modal .menu-icon img:not(.hide)").length == 0;
-    if (isSingleOption & isAllDeselected) {
-        selectSingleOption(menuItem.querySelector(".modal button"));
+    if (modal.classList.contains('single-option')) {
+        // If no option is selected default to the first option
+        if (modal.querySelectorAll('.menu-icon:not(.hidden)').length === 0) {
+            selectSingleOption(modal.querySelector('button'));
+        }
     }
+};
+
+function populateMenuFromSettings() {
+    document.querySelectorAll('.menu-item').forEach((item) => setOptionsForMenu(item.id));
 }
 
-function getMaskFromSubmenu(menuItem) {
-    var val = 0;
-    [].forEach.call(menuItem.querySelectorAll(".modal img:not(.hide)"), function (toggle) {
-            val += parseInt(toggle.dataset.val);
+function getMaskFromMenuID(id) {
+    var value = 0;
+    const modal = document.querySelector(`.modal[data-id='${id}']`);
+
+    const options = modal.querySelectorAll('img:not(.hidden)');
+
+    options.forEach(function (toggle) {
+        value += parseInt(toggle.dataset.val);
     });
-    return val
+
+    return value;
 }
 
-function resetCurrentSubmenu() {
-    var focus = document.querySelector(".menu-item .modal:not(.hide)");
-    if (!focus) {
-        focus = document.querySelector(":focus");
-    }
-    var menuItem = closestClass(focus, "menu-item");
+function resetCurrentMenu() {
+    playSound('SeWebTextboxStartEdit');
+    const menu = document.querySelector('.modal:not(.hide)');
 
-    var key = defaults_prefix + menuItem.id;
-    var section_mask = decodeURIComponent(settings.get(key));
-    setSubmenuByMask(menuItem, section_mask);
+    const menuId = menu.dataset.id;
+    const defaultSectionMask = settings[DEFAULTS_PREFIX + menuId];
+
+    settings[menuId] = defaultSectionMask;
+
+    populateMenuFromSettings();
 }
 
-function resetAllSubmenus() {
+function resetAllMenus() {
     // Resets all submenus to the default values
-    if (confirm("Are you sure that you want to reset all menu settings to the default?")) {
-        [].forEach.call(document.querySelectorAll(".menu-item"), function (menuItem) {
-            var key = defaults_prefix + menuItem.id;
-            var mask = decodeURIComponent(settings.get(key));
-            setSubmenuByMask(menuItem, mask)
+    if (confirm('Are you sure that you want to reset all menu settings to the default?')) {
+        document.querySelectorAll('.menu-item').forEach(function (item) {
+            const defaultMenuId = DEFAULTS_PREFIX + item.id;
+            const defaultMask = settings[defaultMenuId];
+
+            settings[item.id] = defaultMask;
+
+            populateMenuFromSettings();
         });
     }
 }
 
 function setHelpText(text) {
-    // Modify the help text in the footer
-    document.getElementById("help-text").innerText = text;
+    document.getElementById('help-text').innerText = text;
 }
 
 function saveDefaults() {
-    if (confirm("Are you sure that you want to change the default menu settings to the current selections?")) {
-        var key;
-        var mask;
-        [].forEach.call(document.querySelectorAll(".menu-item"), function (menuItem) {
-            key = defaults_prefix + menuItem.id;
-            mask = getMaskFromSubmenu(menuItem);
-            settings.set(key, mask);
+    if (confirm('Are you sure that you want to change the default menu settings to the current selections?')) {
+        document.querySelectorAll('.menu-item').forEach((item) => {
+            const menu = DEFAULTS_PREFIX + item.id;
+
+            settings[menu] = getMaskFromMenuID(item.id);
         });
     }
 }
 
-function isInBitmask(val, mask) {
-    // Return true if the value is in the bitmask
-    return (mask & val) != 0
-}
-
 function cycleNextTab() {
     // Cycle to the next tab
-    var activeTab = document.querySelector(".tab-button.active");
+    const activeTab = document.querySelector('.tab-button.active');
     var nextTab = activeTab.nextElementSibling;
     if (!nextTab) {
         // On the last tab - set the next tab as the first tab in the list
-        nextTab = document.querySelector(".tab-button");
+        nextTab = document.querySelector('.tab-button');
     }
     openTab(nextTab);
 }
 
 function cyclePrevTab() {
     // Cycle to the previous tab
-    var activeTab = document.querySelector(".tab-button.active");
-    var prevTab = activeTab.previousElementSibling;
-    if (!prevTab) {
+    const activeTab = document.querySelector('.tab-button.active');
+    var previousTab = activeTab.previousElementSibling;
+    if (!previousTab) {
         // On the first tab - set the next tab as the last tab in the list
-        tabs = document.querySelectorAll(".tab-button");
-        prevTab = tabs[tabs.length - 1];
+        tabs = document.querySelectorAll('.tab-button');
+        previousTab = tabs[tabs.length - 1];
     }
-    openTab(prevTab);
+    openTab(previousTab);
 }
