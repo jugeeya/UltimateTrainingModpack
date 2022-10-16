7f3a8e8674ce6ebe94add7b77d5c42babfebc7a5
GradualSyrup <68757075+GradualSyrup@users.noreply.github.com>
2022-10-16 18:34:05 -0500

    No Star KOs (#391)
    * Turn off Star KOs
    
    * Remove Star KO Debug

--- src/training/mod.rs
+++ src/training/mod.rs
@@ -460,6 +460,19 @@ pub unsafe fn handle_effect(
     )
 }
 
+static CAN_FUTTOBI_BACK_OFFSET: usize = 0x0260f950; // can_futtobi_back, checks if stage allows for star KOs
+#[skyline::hook(offset = CAN_FUTTOBI_BACK_OFFSET)]
+pub unsafe fn handle_star_ko(
+    my_long_ptr: &mut u64,
+) -> bool {
+    let ori = original!()(my_long_ptr);
+    if !is_training_mode() {
+        return ori;
+    } else {
+        return false;
+    }
+}
+
 #[allow(improper_ctypes)]
 extern "C" {
     fn add_nn_hid_hook(callback: fn(*mut NpadGcState, *const u32));
@@ -540,6 +553,8 @@ pub fn training_mods() {
         handle_se,
         // Death GFX
         handle_effect,
+        // Star KO turn off
+        handle_star_ko,
     );
 
     combo::init();
