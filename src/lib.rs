1c46e66aacae5a41f743ef2897bd076b8adb4311
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2023-01-27 12:58:48 -0500

    Change panic message

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ pub fn main() {
         let err_msg = format!("thread has panicked at '{msg}', {location}");
         skyline::error::show_error(
             69,
-            "Skyline plugin has panicked! Please open the details and send a screenshot to the developer, then close the game.\n",
+            "SSBU Training Modpack has panicked! Please open the details and send a screenshot to the developer, then close the game.\n",
             err_msg.as_str(),
         );
     }));
