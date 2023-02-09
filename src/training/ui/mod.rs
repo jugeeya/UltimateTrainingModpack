3d6ea34eebde0c9e3df56bd11e47f976e972bf5e
jugeeya <jugeeya@live.com>
2023-02-09 07:51:40 -0800

    Use layout.arc to Build UI (#473)
    * Update lib.rs
    
    * Remaining fixes
    
    * Initial
    
    * Forgot to commit
    
    * Clippy
    
    * Initial
    
    * Use feature gate
    
    * Fixes
    
    * layout.arc actually working. Add training_mod_menu_button
    
    * Add docstring, fix from static
    
    * Use include_flate to keep binary smaller
    
    * Add most menu features back
    
    * Add help, tabs
    
    * Add help, tabs
    
    * Small refactor
    
    * Fix clippy, some other issues
    
    * Updated menu buttons, inactive text colour (btns), layouts
    
    ---------
    
    Co-authored-by: Matthew Edell <edell.matthew@gmail.com>

--- src/training/ui/mod.rs
+++ src/training/ui/mod.rs
@@ -1,76 +1,12 @@
 use crate::common::{is_ready_go, is_training_mode};
+use sarc::SarcFile;
 use skyline::nn::ui2d::*;
 use training_mod_consts::{OnOff, MENU};
-use std::collections::HashMap;
-use parking_lot::Mutex;
-use skyline::libc::c_void;
 
 mod damage;
 mod display;
 mod menu;
 pub mod notifications;
-
-type PaneCreationCallback = for<'a, 'b> unsafe fn(&'a str, &'b mut Pane,
-                                                  extern "C" fn(*mut Layout, *mut u8, *const u8, *mut ResPane, *const u8, *const u8, *const u8, u32) -> *mut Pane,
-                                                  *mut Layout, *mut u8, *const u8, *mut ResPane,
-                                                  *const u8, *const u8, *const u8, u32);
-type PaneCreationMap =  HashMap<
-    (String, String), Vec<(bool, PaneCreationCallback)>
->;
-
-lazy_static::lazy_static! {
-    static ref PANE_CREATED: Mutex<PaneCreationMap> = Mutex::new(HashMap::from([
-        (
-            (String::from("info_training"), String::from("pic_numbase_01")),
-            vec![
-                (false, menu::BUILD_CONTAINER_PANE),
-                (false, display::BUILD_PIC_BASE),
-                (false, menu::BUILD_SLIDER_CONTAINER_PANE),
-            ]
-        ),
-        (
-            (String::from("info_training"), String::from("pic_help_bg_00")),
-            vec![(false, menu::BUILD_FOOTER_BG)]
-        ),
-        (
-            (String::from("info_training"), String::from("set_txt_help_00")),
-            vec![(false, menu::BUILD_FOOTER_TXT)]
-        ),
-        (
-            (String::from("info_training"), String::from("set_txt_num_01")),
-            vec![
-                (false, menu::BUILD_TAB_TXTS),
-                (false, menu::BUILD_OPT_TXTS),
-                (false, menu::BUILD_SLIDER_TXTS),
-                (false, display::BUILD_PANE_TXT),
-            ]
-        ),
-        (
-            (String::from("info_training"), String::from("txt_cap_01")),
-            vec![
-                (false, display::BUILD_HEADER_TXT),
-                (false, menu::BUILD_SLIDER_HEADER_TXT),
-            ]
-        ),
-        (
-            (String::from("info_training_btn0_00_item"), String::from("icn_bg_main")),
-            vec![(false, menu::BUILD_BG_LEFTS)]
-        ),
-        (
-            (String::from("info_training_btn0_00_item"), String::from("btn_bg")),
-            vec![(false, menu::BUILD_BG_BACKS)]
-        ),
-    ]));
-}
-
-pub unsafe fn reset_creation() {
-    let pane_created = &mut *PANE_CREATED.data_ptr();
-    pane_created.iter_mut().for_each(|(_identifier, creators)| {
-        creators.iter_mut().for_each(|(created, _callback)| {
-            *created = false;
-        })
-    })
-}
 
 #[skyline::hook(offset = 0x4b620)]
 pub unsafe fn handle_draw(layout: *mut Layout, draw_info: u64, cmd_buffer: u64) {
@@ -94,76 +30,102 @@ pub unsafe fn handle_draw(layout: *mut Layout, draw_info: u64, cmd_buffer: u64) 
     original!()(layout, draw_info, cmd_buffer);
 }
 
-#[skyline::hook(offset = 0x493a0)]
-pub unsafe fn layout_build_parts_impl(
-    layout: *mut Layout,
-    out_build_result_information: *mut u8,
-    device: *const u8,
-    block: *mut ResPane,
-    parts_build_data_set: *const u8,
-    build_arg_set: *const u8,
-    build_res_set: *const u8,
-    kind: u32,
-) -> *mut Pane {
-    let layout_name = &skyline::from_c_str((*layout).layout_name);
-    let root_pane = &mut *(*layout).root_pane;
+// We'll keep some sane max size here; we shouldn't reach above 600KiB is the idea,
+// but we can try higher if we need to.
+#[cfg(feature = "layout_arc_from_file")]
+static mut LAYOUT_ARC : &mut [u8; 600000] = &mut [0u8; 600000];
 
-    let block_name = (*block).get_name();
-    let identifier = (layout_name.to_string(), block_name);
-    let pane_created = &mut *PANE_CREATED.data_ptr();
-    let panes = pane_created.get_mut(&identifier);
-    if let Some(panes) = panes {
-        panes.iter_mut().for_each(|(has_created, callback)| {
-            if !*has_created {
-                callback(layout_name,
-                         root_pane,
-                         original!(),
-                         layout,
-                         out_build_result_information,
-                         device,
-                         block,
-                         parts_build_data_set,
-                         build_arg_set,
-                         build_res_set,
-                         kind
-                );
-
-                // Special case: Menu init should always occur
-                if ("info_training".to_string(), "pic_numbase_01".to_string()) != identifier {
-                    *has_created = true;
-                }
-            }
-        });
+/// We are editing the info_training/layout.arc and replacing the original file with our
+/// modified version from `sd://TrainingModpack/layout.arc` or, in the case of Ryujinx for the cool
+/// kids `${RYUJINX_DIR}/sdcard/TrainingModpack/layout.arc`
+///
+/// When we edit the layout we are doing two things.
+///
+/// 1. Creating a new BFLYT inside the layout.arc for whatever component we are making. For example,
+/// the slider menu button.
+///
+/// 2. Adding a Parts pane to the info_training.bflyt with the "Part Name" matching the name of
+/// our new BFLYT without the file extension (mimicking how it's done with native Parts panes)
+///
+/// # Warnings
+/// When creating a BFLYT from an existing one we need to edit the names of the panes so that
+/// the default animations no longer modify them.  Otherwise the game will override properties,
+/// i.e. material colours, and we won't be able to control them properly.
+///
+/// Once we have the file edited and saved to the correct location we can access the pane
+/// from the layout as we normally would in our Draw function
+/// `(root_pane.find_pane_by_name_recursive("name_of_parts_pane")`.
+///
+/// # Usage
+/// Now say I want to edit background colour of the button's label.
+/// I would have to grab the parts pane and find the pane I want to modify on it, then I'd be able
+/// to make the modifications as I normally would.
+///
+/// ```rust
+/// let slider_button = root_pane.find_pane_by_name_recursive("name_of_parts_pane");
+/// let label_bg = slider_button.find_pane_by_name_recursive("name_of_picture_pane");
+///
+/// let label_material = &mut *label_bg.as_picture().material;
+///
+/// label_material.set_white_res_color(LABEL_WHITE_SELECTED_COLOR);
+/// label_material.set_black_res_color(LABEL_BLACK_SELECTED_COLOR);
+/// ```
+#[skyline::hook(offset = 0x37730d4, inline)]
+unsafe fn handle_layout_arc_malloc(
+    ctx: &mut skyline::hooks::InlineCtx
+) {
+    if !is_training_mode() {
+        return;
     }
 
-    original!()(
-        layout,
-        out_build_result_information,
-        device,
-        block,
-        parts_build_data_set,
-        build_arg_set,
-        build_res_set,
-        kind,
-    )
-}
+    let decompressed_file = *ctx.registers[21].x.as_ref() as *const u8;
+    let decompressed_size = *ctx.registers[1].x.as_ref() as usize;
 
+    let layout_arc = SarcFile::read(
+        std::slice::from_raw_parts(decompressed_file,decompressed_size)
+    ).unwrap();
+    let training_layout = layout_arc.files.iter().find(|f| {
+        f.name.is_some() && f.name.as_ref().unwrap() == &String::from("blyt/info_training.bflyt")
+    });
+    if training_layout.is_none() {
+        return;
+    }
 
-#[skyline::hook(offset = 0x32cace0)]
-pub unsafe fn handle_load_layout_files(
-    meta_layout_root: *mut c_void,
-    loading_list: *mut c_void,
-    layout_arc_hash: *const u32,
-    param_4: i32
-) -> u64 {
-    println!("Layout.arc hash: {:x}", *layout_arc_hash);
-    original!()(meta_layout_root, loading_list, layout_arc_hash, param_4)
+    let inject_arc;
+    let inject_arc_size : u64;
+
+    #[cfg(feature = "layout_arc_from_file")] {
+        let inject_arc_from_file = std::fs::read("sd:/TrainingModpack/layout.arc").unwrap();
+        inject_arc_size = inject_arc_from_file.len() as u64;
+
+        // Copy read file to global
+        inject_arc_from_file
+            .iter()
+            .enumerate()
+            .for_each(|(idx, byte)| LAYOUT_ARC[idx] = *byte);
+        inject_arc = LAYOUT_ARC.as_ptr();
+    }
+
+    #[cfg(not(feature = "layout_arc_from_file"))] {
+        include_flate::flate!(static INJECT_ARC_FROM_FILE: [u8] from "src/static/layout.arc");
+
+        inject_arc = INJECT_ARC_FROM_FILE.as_ptr();
+        inject_arc_size = INJECT_ARC_FROM_FILE.len() as u64;
+    }
+
+    // Decompressed file pointer
+    let decompressed_file = ctx.registers[21].x.as_mut();
+    *decompressed_file = inject_arc as u64;
+
+    // Decompressed size is in each of these registers
+    *ctx.registers[1].x.as_mut() = inject_arc_size;
+    *ctx.registers[23].x.as_mut() = inject_arc_size;
+    *ctx.registers[24].x.as_mut() = inject_arc_size;
 }
 
 pub fn init() {
     skyline::install_hooks!(
         handle_draw,
-        layout_build_parts_impl,
-        // handle_load_layout_files
+        handle_layout_arc_malloc
     );
 }
