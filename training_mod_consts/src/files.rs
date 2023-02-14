fffcd9f688b30b7e83cd878a15282c87ca1795e9
jugeeya <jugeeya@live.com>
2023-02-14 01:33:45 +0000

    Initial

---
+++ training_mod_consts/src/files.rs
@@ -0,0 +1,4 @@
+const ULTIMATE_ROOT: &str = "sd:/ultimate/";
+
+const TRAINING_MODPACK_ROOT: &str = &(ULTIMATE_ROOT.to_string() + "TrainingModpack");
+const TRAINING_MODPACK_TOML: &str = &(TRAINING_MODPACK_ROOT.to_string() + "/training_modpack.toml");
