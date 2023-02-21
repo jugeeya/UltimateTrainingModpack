4570c70c6440db3e09d9a5d181048cec9d7d756c
jugeeya <jugeeya@live.com>
2023-02-20 17:21:46 -0800

    Avoid making layouts invisible outside training mode with HUD off

--- src/training/ui/mod.rs
+++ src/training/ui/mod.rs
@@ -22,6 +22,8 @@ pub unsafe fn handle_draw(layout: *mut Layout, draw_info: u64, cmd_buffer: u64) 
         // InfluencedAlpha means "Should my children panes' alpha be influenced by mine, as the parent?"
         root_pane.flags |= 1 << PaneFlag::InfluencedAlpha as u8;
         root_pane.set_visible(MENU.hud == OnOff::On);
+    } else {
+        root_pane.set_visible(true);
     }
 
     damage::draw(root_pane, layout_name);
