2b17a4044cbd0517cd6eb20a6ea4634da5daa8a0
GradualSyrup <68757075+GradualSyrup@users.noreply.github.com>
2023-08-10 17:14:00 -0500

    fmt

--- src/training/input_record.rs
+++ src/training/input_record.rs
@@ -183,9 +183,7 @@ pub unsafe fn get_command_flag_cat(module_accessor: &mut BattleObjectModuleAcces
         if button_config::combo_passes_exclusive(button_config::ButtonCombo::InputPlayback) {
             playback(MENU.playback_button_combination.get_random().into_idx());
         } else if MENU.record_trigger.contains(RecordTrigger::COMMAND)
-            && button_config::combo_passes_exclusive(
-                button_config::ButtonCombo::InputRecord,
-            )
+            && button_config::combo_passes_exclusive(button_config::ButtonCombo::InputRecord)
         {
             lockout_record();
         }
