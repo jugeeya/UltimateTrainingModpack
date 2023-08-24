b519152ce7dc3fe471f25a1375218aaf5569e9f3
jugeeya <jugeeya@live.com>
2023-08-24 09:05:37 -0700

    Add space to dialog notification for release

--- src/common/release.rs
+++ src/common/release.rs
@@ -210,7 +210,7 @@ pub fn perform_version_check() {
                 info!("User declined the update.");
                 dialog::dialog_ok(
                     "This update has been declined.\n\n\
-                    If you'd like to disable update checking in the future,\
+                    If you'd like to disable update checking in the future, \
                     please adjust the 'Auto-Update' setting in the Modpack menu."
                         .to_string(),
                 );
