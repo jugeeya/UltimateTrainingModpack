46d5854287f5482d0704794e2d61be910564ce36
Matthew Edell <edell.matthew@gmail.com>
2023-09-14 07:12:34 -0400

    Added missing icon names to list, fixed missing trailing comma

--- src/training/ui/input_log.rs
+++ src/training/ui/input_log.rs
@@ -124,7 +124,15 @@ unsafe fn draw_log(root_pane: &Pane, log_idx: usize, log: &InputLog) {
         "dpad_down",
         "dpad_left",
         "dpad_right",
-        "dpad_left_right"
+        "dpad_left_right",
+        "up_strong",
+        "down_strong",
+        "left_strong",
+        "right_strong",
+        "up_left_strong",
+        "down_left_strong",
+        "up_right_strong",
+        "down_right_strong",
     ];
 
     for idx in 0..NUM_ICON_SLOTS {
