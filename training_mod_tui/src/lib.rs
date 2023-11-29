bcb8f9e44f323b4cc5cedbd950e494744e3def55
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2023-11-29 18:20:35 -0500

    Remove unneeded command line interface

--- training_mod_tui/src/lib.rs
+++ training_mod_tui/src/lib.rs
@@ -3,8 +3,5 @@ pub use containers::*;
 mod structures;
 pub use structures::*;
 
-pub mod ui;
-pub use crate::ui::*;
-
 pub const NX_SUBMENU_ROWS: usize = 8;
 pub const NX_SUBMENU_COLUMNS: usize = 4;
