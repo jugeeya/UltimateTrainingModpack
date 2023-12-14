f67aef407b91bc456eb74ba1abf49194c659023c
jugeeya <jugeeya@live.com>
2023-12-13 17:43:14 -0800

    Fix test

--- training_mod_tui/tests/test_submenu.rs
+++ training_mod_tui/tests/test_submenu.rs
@@ -54,9 +54,6 @@ fn initialize_submenu<'a>(submenu_type: SubMenuType) -> SubMenu<'a> {
             submenu_type: submenu_type,
             toggles: make_toggle_table_multiple(0, 0, 0),
             slider: Some(StatefulSlider::new()),
-        },
-        SubMenuType::None => {
-            panic!()
         }
     }
 }
