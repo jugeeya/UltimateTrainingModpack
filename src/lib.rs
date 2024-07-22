29f1d5b68ef47a262c3a8ef556da5c32cc841f78
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2024-07-21 18:29:34 -0700

    Fix clippy warnings (#695)

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@
     clippy::missing_safety_doc,
     clippy::wrong_self_convention,
     clippy::option_map_unit_fn,
-    clippy::transmute_num_to_bytes
+    clippy::transmute_num_to_bytes,
+    clippy::missing_transmute_annotations
 )]
 
 use std::fs;
