50b739b7d29c61307b7299821c93c34a351cbcbc
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2026-09-04 09:13:16 -0700

    Update for 13.0.5 (#771)

--- src/training/mod.rs
+++ src/training/mod.rs
@@ -445,7 +445,7 @@ unsafe fn stale_handle(ctx: &mut InlineCtx) {
 #[skyline::hook(offset = *OFFSET_STALE_MENU, inline)]
 unsafe fn stale_menu_handle(ctx: &mut InlineCtx) {
     // Set the text pointer to where "mel_training_on" is located
-    let on_text_ptr = (getRegionAddress(Region::Text) as u64) + 0x42b315e;
+    let on_text_ptr = (getRegionAddress(Region::Text) as u64) + 0x42b415e;
     ctx.registers[1].set_x(on_text_ptr);
 }
 
